98c66492beaa206a6802e3982ea458592190e544
Luiz Gustavo de Souza Vargas <luizgustavovargas28@gmail.com>
2023-03-12 17:18:40 -0300

    :lipstick: feat(style): Criação inicial da página home

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  display: flex;
+  min-height: 90vh;
+  width: 100%;
+
+  background-color: red;
+  position: relative;
+
+  padding: 200px 0px;
+`;
