41d3a56f5b5612e2ac6dd31dc1eb8fc099a41888
Luiz Gustavo de Souza Vargas <luizgustavovargas28@gmail.com>
2023-04-20 20:55:40 -0300

    :sparkles: feat: Menu de navegação (desktop/mobile)

--- src/components/MenuHeader/styles.ts
+++ src/components/MenuHeader/styles.ts
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 
-export const Container = styled.header`
+interface IMenu {
+  menu: boolean;
+}
+
+export const Container = styled.header<IMenu>`
   display: flex;
   flex-direction: row;
 
+  grid-area: HD;
+
   height: 14vh;
-  width: 100%;
+  width: 100vw;
 
   background-color: ${({ theme }) => theme.background};
 
@@ -25,8 +31,7 @@ export const Container = styled.header`
     flex-direction: row;
 
     align-items: center;
-
-    gap: 14rem;
+    justify-content: space-between;
 
     width: 100%;
     height: auto;
@@ -36,6 +41,7 @@ export const Container = styled.header`
 
       svg {
         height: 1.75rem;
+        width: auto;
       }
     }
 
@@ -50,9 +56,167 @@ export const Container = styled.header`
       list-style-type: none;
 
       li {
+        font-size: 1rem;
         color: ${({ theme }) => theme.title};
         font-family: "Archivo", sans-serif;
+        
+        white-space: nowrap;
+
+        cursor: pointer;
       }
     }
+
+    button.button-menu {
+      display: none;
+
+      align-items: center;
+      justify-content: center;
+
+      border: none;
+      border-radius: 0.3rem;
+
+      padding: 0.3rem;
+
+      background-color: ${({ theme }) => theme.details};
+
+      svg {
+        fill: ${({ theme }) => theme.white};
+      }
+    }
+  }
+
+  aside {
+    display: ${({ menu }) => (menu === true ? "flex" : "none")};
+    flex-direction: column;
+
+    gap: 4rem;
+
+    padding: 1.5rem;
+
+    background: ${({ theme }) => theme.details};
+
+    position: absolute;
+    right: 0;
+
+    height: 100vh;
+    width: 16rem;
+
+    header {
+      display: flex;
+
+      align-items: center;
+      justify-content: center;
+
+      padding: 0.5rem;
+
+      border-bottom: 1px solid ${({ theme }) => theme.gray};
+
+      h4 {
+        font-size: 1rem;
+        font-weight: 400;
+        color: ${({ theme }) => theme.title};
+      }
+    }
+
+    section {
+      display: flex;
+      flex-direction: column;
+
+      gap: 1rem;
+
+      a {
+        text-decoration: none;
+        color: ${({ theme }) => theme.subTitle};
+
+        cursor: pointer;
+
+      }
+    }
+
+    footer {
+      display: flex;
+
+      align-items: center;
+      justify-content: center;
+    }
+  }
+
+  /* Mobile */
+  @media screen and (max-width: 480px) {
+
+    padding: 0px 1.5rem;
+
+    section {
+      svg {
+        height: 1rem;
+      }
+    }
+    
+    nav {
+      ul {
+        display: none;
+      }
+      button.button-menu {
+        display: flex;
+      }
+    }
+
+  }
+  /* Largura mobile e tablet pequeno */
+  @media screen and (min-width: 481px) and (max-width: 768px) {
+
+    padding: 0px 2rem;
+
+    section {
+      svg {
+        height: 1rem;
+      }
+    }
+
+    nav {
+      ul {
+        display: none;
+      }
+      button.button-menu {
+        display: flex;
+      }
+    }
+  }
+  /* Tablets */
+  @media screen and (min-width: 769px) and (max-width: 1024px) {
+    section {
+      svg {
+        height: 1rem;
+      }
+    }
+
+    nav {
+      ul {
+        li {
+          font-size: 0.75rem;
+        }
+        gap: 2rem;
+      }
+    }
+  }
+  /* Desktop e Laptops */
+  @media screen and (min-width: 1025px) and (max-width: 1280px) {
+    section {
+      svg {
+        height: 1rem;
+      }
+    }
+
+    nav {
+      ul {
+        li {
+          font-size: 1rem;
+        }
+        gap: 2rem;
+      }
+    }
+  }
+  /* Largura desktop */
+  @media screen and (min-width: 1281px) {
   }
 `;
