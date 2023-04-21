d2706e341f672c5846f8148d9f97cdc8d7e31d04
Luiz Gustavo de Souza Vargas <luizgustavovargas28@gmail.com>
2023-04-20 21:00:49 -0300

    :sparkles: Hook de exportação de ícones e imagens

--- src/assets/export/index.ts
+++ src/assets/export/index.ts
@@ -1,4 +1,5 @@
-import {ReactComponent as LogoSvg}  from '../img/Logo.svg';
-import {ReactComponent as IllustrationDeveloper}  from '../img/IllustrationDeveloper.svg';
-
-export { LogoSvg, IllustrationDeveloper };
+export {ReactComponent as LogoSvg}  from '../img/Logo.svg';
+export {ReactComponent as IllustrationDeveloper}  from '../img/IllustrationDeveloper.svg';
+export {ReactComponent as MenuIcon}  from '../icons/menu.svg';
+export {ReactComponent as Profile}  from '../img/Profile.svg';
+export {ReactComponent as DownloadIcon}  from '../icons/download.svg';
